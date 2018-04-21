2dba81f0159ba1067df4b6c5c4437ccf4e96b812
Pablo <pablorsimplicio@gmail.com>
2018-04-21 03:14:19 -0300

    iniciando repositório

---
+++ src/main/java/br/com/pmp/referencia/controller/LoginController.java
@@ -0,0 +1,46 @@
+package br.com.pmp.referencia.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.pmp.referencia.dao.JdbcUsuarioDao;
+import br.com.pmp.referencia.model.Usuario;
+
+@Controller
+public class LoginController {
+	
+	private JdbcUsuarioDao dao;
+	
+	@Autowired
+	public LoginController(JdbcUsuarioDao dao) {
+		
+		this.dao = dao;
+		
+	}
+	
+	
+	@GetMapping("/")
+	public String login() {
+		
+		return "loginForm";
+	}
+	
+	@RequestMapping("/efetuarLogin")
+	public String efetuarLogin(Usuario usuario, HttpSession session) {
+		
+		if(dao.existeUsuario(usuario)) {
+			
+			session.setAttribute("usuarioLogado", usuario);
+			return "menu";
+			
+		}
+		
+		return "redirect:/";
+		
+	}
+
+}
---
+++ src/main/java/br/com/pmp/referencia/dao/JdbcUsuarioDao.java
@@ -0,0 +1,60 @@
+package br.com.pmp.referencia.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import br.com.pmp.referencia.model.Usuario;
+
+@Repository
+public class JdbcUsuarioDao {
+	
+	private Connection connetion;
+	
+	@Autowired
+	public JdbcUsuarioDao(BasicDataSource dataSource) {
+		
+		try {
+			this.connetion = dataSource.getConnection();
+		} catch (SQLException e) {
+			
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	public boolean existeUsuario(Usuario usuario) {
+		
+		String sql = "select * from usuarios where login = ? and senha = ?";
+		PreparedStatement stmt;
+		
+		try {
+			
+			stmt = connetion.prepareStatement(sql);
+			stmt.setString(1, usuario.getLogin());
+			stmt.setString(2, usuario.getSenha());
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				
+				return true;
+			}
+			
+			return false;
+			
+		} catch (SQLException e) {
+			
+			throw new RuntimeException(e);
+			
+		}
+		
+		
+	}
+
+}
